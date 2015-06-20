bd4198ebb8af1959d4fbb7b0728e116f22265588
aonotas <nanigashi03@gmail.com>
2015-06-20 23:46:10 +0900

    bug fix

--- plot_result.py
+++ plot_result.py
@@ -13,13 +13,21 @@ def log_file(filename):
     test_set  = []
     for l in open(filename):
         v = l.split(":")
-        if " train_set" in l:
-            train_set.append(float(v[1]))
-        if " dev_set" in l:
-            dev_set.append(float(v[1]))
-        if " test_set" in l:
-            test_set.append(float(v[1]))
-    return train_set, dev_set, test_set
+        train_flag = " train_set" in l
+        dev_flag   = " dev_set"   in l
+        test_flag  = " test_set"  in l
+        if len(v) == 2 and (train_flag or dev_flag or test_flag):
+            value = float(v[1])
+        else:
+            continue
+        if train_flag:
+            train_set.append(value)
+        if dev_flag:
+            dev_set.append(value)
+        if test_flag:
+            test_set.append(value)
+        min_len = min(len(train_set), len(dev_set), len(test_set))
+    return train_set[:min_len], dev_set[:min_len], test_set[:min_len]
 
 def main():
     argvs = sys.argv
@@ -31,12 +39,14 @@ def main():
     print "test_best", max(test_set)
 
     x = range(len(train_set))
+    plt.ylim([0.0, 1.0])
     plt.plot(x, train_set, "b", linewidth=2, alpha=0.8, label="train")
     plt.plot(x, dev_set,   "r", linewidth=2, alpha=0.8, label="dev")
     plt.plot(x, test_set,  "g", linewidth=2, alpha=0.8, label="test")
     plt.legend(loc='lower right')
     # plt.show()
     print save_filename
+
     plt.savefig(save_filename)
     plt.show()
 
